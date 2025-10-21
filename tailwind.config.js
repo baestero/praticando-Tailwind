4a060da08edb5344185f65e60542654b3c5a2e45
baestero <leonardobaestero@gmail.com>
2025-10-21 16:44:27 -0300

    inicial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"], // inclui todos os HTMLs dentro de src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
