f93f0d69f4125ab62e229a592b9fd4495b960ad3
baestero <leonardobaestero@gmail.com>
2025-11-04 20:14:50 -0300

    feat: adicionar nova atividade de grid com meu desafio realizado

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.html"], // inclui todos os HTMLs dentro de src
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
